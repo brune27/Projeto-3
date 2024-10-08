67b8f24eded4efa7f99cebc631c91762d771d640
brune27 <bruno.bueno24@escola.pr.gov.br>
2024-10-08 14:52:07 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,1 +1,21 @@
-
+criaCartao(
+  'programação'
+  'O que é JavaScript',
+  'O javascript é uma linguagem de programação'
+)
+CriaCartao(
+  'Geografia',
+  'Qual a capital da França?',
+  'A capital da França é Paris'
+  )
+criaCartao(
+  'programação',
+  'O que é uma Função?
+  'Uma função é um bloco de código que executa
+  )
+criarCartao(
+  'Lingua inglesa'
+  'Como se diz oi em inglês'
+'Oi em inglês se diz Hi (Rai)'
+)
+  
